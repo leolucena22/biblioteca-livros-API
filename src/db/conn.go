4f9db779c56c3748fa2d5558cd817acb42ade135
leolucena22 <leonardolucena44@gmail.com>
2025-04-16 17:00:08 -0300

    Add db connnec and model

--- src/db/conn.go
+++ src/db/conn.go
@@ -1,1 +1,36 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "postgres"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", sqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to database" + dbname)
+
+	return db, nil
+}
--- src/model/Biblioteca.go
+++ src/model/Biblioteca.go
@@ -1,1 +1,12 @@
 package model
+
+import "time"
+
+type Book struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Title     string    `json:"title" validate:"required,min=3"`
+	ISBN      string    `json:"isbn" validate:"required,isbn"`
+	Year      int       `json:"year" validate:"gte=1900"`
+	AuthorID  uint      `json:"author_id"` // Para relacionamento
+	CreatedAt time.Time `json:"created_at"`
+}
